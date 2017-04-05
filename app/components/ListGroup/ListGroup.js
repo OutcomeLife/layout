758a00162ca708097269d80663ecaaca045bf1fc
Nischal Gautam <nischalgautam@Nischals-MBP.lan>
2017-04-05 17:33:36 +1000

    added javscript to index to merge with keycloak backend

---
+++ app/components/ListGroup/ListGroup.js
@@ -0,0 +1,26 @@
+import react, {Component} from 'react';
+
+class ListGroup extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(
+    return(
+      <div className ="list-group">
+        <h3> Recent Tickers </h3>
+        <ul class="list-group">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
+    );
+  )
+
+}
+
+
+export default ListGroup;
