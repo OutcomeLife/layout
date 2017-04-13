f77b2fc97466d2463fd64ebb4f5b7c4a4872dd78
prakash <unique_prakash2002@yahoo.com>
2017-04-13 10:42:03 +1000

    starting as npm package

---
+++ dist/entry.js
@@ -0,0 +1,16 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _App = require('./App.js');
+
+Object.defineProperty(exports, 'App', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_App).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
