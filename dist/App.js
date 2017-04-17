383051bba0a7f3e6692b2ec223aa8f7f0310526d
prakash <unique_prakash2002@yahoo.com>
2017-04-18 09:56:27 +1000

    fixed minor issue

--- dist/App.js
+++ dist/App.js
@@ -10,6 +10,8 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _entry = require('./entry');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -31,9 +33,48 @@ var App = function (_Component) {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        'h1',
-        null,
-        ' hello '
+        'div',
+        { className: 'intern' },
+        _react2.default.createElement(_entry.Header, null),
+        _react2.default.createElement(
+          _entry.Body,
+          null,
+          _react2.default.createElement(
+            _entry.Sidebar,
+            null,
+            _react2.default.createElement(
+              'a',
+              { href: '#' },
+              'Roles '
+            ),
+            _react2.default.createElement(
+              'a',
+              { href: '#' },
+              'Message '
+            ),
+            _react2.default.createElement(
+              'a',
+              { href: '#' },
+              'Contact '
+            ),
+            _react2.default.createElement(
+              'a',
+              { href: '#' },
+              'Admin '
+            )
+          ),
+          _react2.default.createElement(
+            _entry.Content,
+            null,
+            'content'
+          )
+        ),
+        '*/}',
+        _react2.default.createElement(
+          _entry.Footer,
+          null,
+          'Outcome.life Jenny layout'
+        )
       );
     }
   }]);
--- dist/entry.js
+++ dist/entry.js
@@ -4,12 +4,48 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _App = require('./App.js');
+var _header = require('./header');
 
-Object.defineProperty(exports, 'App', {
+Object.defineProperty(exports, 'Header', {
   enumerable: true,
   get: function get() {
-    return _interopRequireDefault(_App).default;
+    return _interopRequireDefault(_header).default;
+  }
+});
+
+var _sidebar = require('./body/sidebar');
+
+Object.defineProperty(exports, 'Sidebar', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_sidebar).default;
+  }
+});
+
+var _content = require('./body/content');
+
+Object.defineProperty(exports, 'Content', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_content).default;
+  }
+});
+
+var _body = require('./body');
+
+Object.defineProperty(exports, 'Body', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_body).default;
+  }
+});
+
+var _footer = require('./footer');
+
+Object.defineProperty(exports, 'Footer', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_footer).default;
   }
 });
 
