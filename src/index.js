e0fd3eedff6f1b50612ec39d8e748cfdb9115229
prakash <unique_prakash2002@yahoo.com>
2017-04-21 11:02:25 +1000

    added keycloak.json file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Keycloak from 'keycloak-js';
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
+
+const kc = Keycloak('./keycloak.json');
+
+ kc.init({ onLoad: 'login-required' }).success(function (authenticated) {
+      kc.loadUserInfo().success((user) =>  alert("Welcome " + user.given_name + " to genny"));
+          console.log(kc);
+    }).error(function () {
+      alert('failed to initialize');
+    });
+    
+      console.log(kc);
