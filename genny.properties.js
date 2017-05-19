53beb432afb4f170ebe32a0698f6c354b6c8dc3c
Adam Crow <adamcrow63@gmail.com>
2017-05-19 12:29:15 +1000

    Added export variables parameter feature. REACT_APP_ ....

--- genny.properties.js
+++ genny.properties.js
@@ -1,4 +1,4 @@
 module.exports = {
-    project_name: "PROJECT NAME",
-    qwanda_api_url: "https://qwanda-service.outcome-hub.com"
-};
+    REACT_APP_PROJECT_NAME: "Gidday"
+    REACT_APP_QWANDA_API_URL: "https://qwanda-service.outcome-hub.com"
+}
